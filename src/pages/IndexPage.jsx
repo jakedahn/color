313479322bda5b1fbae8778abd04b2e233efd550
jakedahn <jake@markupisart.com>
2020-02-22 22:29:37 -0700

    adding google analytics

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -3,11 +3,7 @@ import ColorGrid from 'Components/ColorGrid.jsx'
 import colors from './color-data.js'
 
 import Header from 'Components/Header.jsx'
-export const IndexPage = ({ data }) => {
-  // const { ui, entity: taxes } = useSelector(state => state.taxes)
-  // useEffect(() => { window.analytics.page('IndexPage') }, [data])
-
-
+export const IndexPage = () => {
   return (
     <div className="index-page">
       <Header />
