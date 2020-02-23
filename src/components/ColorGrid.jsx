c13125435ff3b80dbba0c6dff54542c4ce0b8a40
jakedahn <jake@markupisart.com>
2020-02-22 19:49:07 -0700

    adding a random url

--- src/components/ColorGrid.jsx
+++ src/components/ColorGrid.jsx
@@ -4,6 +4,54 @@ import ColorCard from './ColorCard.jsx'
 
 import colors from 'Pages/color-data.js'
 
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import {ApolloClient, InMemoryCache, gql, useQuery} from '@apollo/client';
+
+// initialize a GraphQL client
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: 'https://graphql.color.lol',
+  name: `react-${process.env.APOLLO_CLIENT_NAME}`
+});
+
+// write a GraphQL query that asks for names and codes for all countries
+const LIST_COUNTRIES = gql`
+query {
+  palettes {
+    code
+    colors {
+      position
+      hex
+    }
+    likes
+    created_at
+    updated_at
+
+  }
+}`;
+
+// create a component that renders a select input for coutries
+function CountrySelect() {
+  const [country, setCountry] = useState('US');
+  const {data, loading, error} = useQuery(LIST_COUNTRIES, {client});
+
+  if (loading || error) {
+    return <p>{error ? error.message : 'Loading...'}</p>;
+  }
+
+  return (
+    <select value={country} onChange={event => setCountry(event.target.value)}>
+      {data.countries.map(country => (
+        <option key={country.code} value={country.code}>
+          {country.name}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+
 export const ColorGrid = ({ data }) => {
   return (
     <div className="color-grid row">
